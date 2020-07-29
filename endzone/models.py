dfff138faed34f9d5c120a071e6907124a5bc3ad
pantojaa <48775213+pantojaa@users.noreply.github.com>
2020-07-29 13:05:20 -0400

    Update models.py
    Added Event and EventPost models

--- endzone/models.py
+++ endzone/models.py
@@ -14,8 +14,10 @@ class User(db.Model, UserMixin):
     last_name = db.Column(db.String(20), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)  # unique=True means that every email will be unique
     img_file = db.Column(db.String(20), nullable=False, default='default.png')
+    banner_file = db.Column(db.String(20), nullable=False, default='soccerball.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
+    events = db.relationship('Event', backref='author', lazy=True)
     gender = db.Column(db.String(20), nullable=False)
     high_school = db.Column(db.String(20))
     sport = db.Column(db.String(20), nullable=False)
@@ -25,8 +27,10 @@ class User(db.Model, UserMixin):
     birth_year = db.Column(db.String(20), nullable=False)
     age = db.Column(db.Integer, nullable=False)
     about = db.Column(db.String(300), nullable=False, default=' ')
-    # --------------
+    city = db.Column(db.String(30), nullable=False)
+    state = db.Column(db.String(30), nullable=False)
 
+    # --------------
     def __repr__(self):  # how object is printed to console
         return f"User('{self.first_name}', '{self.last_name}', '{self.email}', '{self.img_file}')"
 
@@ -39,6 +43,33 @@ class Post(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     post_image = db.Column(db.String(20), nullable=False, default='temporary.png')
     post_video = db.Column(db.String(20), nullable=False, default='placeholder.mp4')
+    likes = db.Column(db.String(20), default='0')
 
     def __repr__(self):  # how object is printed to console
         return f"Post('{self.title}', '{self.date_posted}')"
+
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    title = db.Column(db.String(120), nullable=False)
+    date = db.Column(db.String(50), nullable=False)
+    time = db.Column(db.String(30), nullable=False)
+    location = db.Column(db.String(60), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    posts = db.relationship('EventPost', backref='post', lazy=True)
+
+    def __repr__(self):  # how object is printed to console
+        return f"Event('{self.title}', '{self.date}', '{self.time}', '{self.location}', '{self.content}', '{self.user_id}')"
+
+
+class EventPost(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    event_id = db.Column(db.Integer, db.ForeignKey('event.id'), nullable=False)
+    post_image = db.Column(db.String(20), nullable=False, default='temporary.png')
+    post_video = db.Column(db.String(20), nullable=False, default='placeholder.mp4')
+
